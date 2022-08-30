9c793a680d03ce99c37838119577f95113232f42
Troels Lund <trolund@gmail.com>
2022-08-30 13:00:38 +0200

    Initial commit

---
+++ Task.py
@@ -0,0 +1,9 @@
+class Task:
+    
+    def __init__(self, name, duration, period, type, priority, deadline):
+        self.name = name
+        self.duration = duration
+        self.period = period
+        self.type = type
+        self.priority = priority
+        self.deadline = deadline
