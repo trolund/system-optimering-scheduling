f8b0cbe1af7e67a95d79b976c742dce5b453c825
SRNielson <nielson1996@hotmail.com>
2022-10-25 10:59:55 +0200

    fixed tests for alg2

--- sa_scheduling/test_schedulability.py
+++ sa_scheduling/test_schedulability.py
@@ -4,25 +4,32 @@ from task import Task
 from cost_functions import calculate_schedulabiltiy
 
 class CheckSchedulabilityTests(unittest.TestCase):
-
-    #define ET subsets and polling server tasks here
-    # create task set
-    task0 = Task("tET0", 1, 3, TaskType.EVENT, 7, 3) 
-    task1 = Task("tET1", 2, 4, TaskType.EVENT, 7, 4)
-    et_subset = [task0, task1]
-
-    #ps = name, duration, period, tasktype, priority, deadline, et_subset
-    ps0 = Task("tTTps0", 5, 3, TaskType.TIME, 1, 3, et_subset)
-
-    #Create tasks that are not schedulable, expect 
-    def isNotSchedulable(self):
-        is_schedulable, result_dict = calculate_schedulability(ps0)
-        self.print(is_schedulable)
     
+    #subset is not schedulable 
+    def testIsNotSchedulable(self):
+        
+        #name, duration, period, tasktype, priority, deadline
+        task0 = Task("tET0", 1, 3, TaskType.EVENT, 7, 3) 
+        task1 = Task("tET1", 2, 4, TaskType.EVENT, 7, 4)
+        et_subset = [task0, task1]
 
-    #Create tasks that are schedulable
-    def isSchedulable(self):
-        return "Not implemented"
+        #ps = name, duration, period, tasktype, priority, deadline, et_subset
+        ps0 = Task("tTTps0", 5, 3, TaskType.TIME, 1, 3, et_subset)
+
+        is_schedulable, result_dict = calculate_schedulabiltiy(ps0)
+        self.assertEqual(is_schedulable, True)
+
+    #subset is schedulable
+    def testIsSchedulable(self):
+        task0 = Task("tET0", 1, 3, TaskType.EVENT, 8, 1) 
+        task1 = Task("tET1", 2, 4, TaskType.EVENT, 7, 4)
+        et_subset = [task0, task1]
+
+        #ps = name, duration, period, tasktype, priority, deadline, et_subset
+        ps0 = Task("tTTps0", 10000, 100000, TaskType.TIME, 7, 1, et_subset)
+
+        is_schedulable, result_dict = calculate_schedulabiltiy(ps0)
+        self.assertEqual(is_schedulable, False)
 
 
 if __name__ == '__main__':
